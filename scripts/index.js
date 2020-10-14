7799e5ba35911e523e9ce89f3725362aad4c0a92
Peppe-Quint <peppequintb@gmail.com>
2020-10-14 12:40:13 +0200

    hamburger button

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,7 @@
 console.log('index.js');
+
+const hamburger = document.querySelector('.header-button');
+
+hamburger.addEventListener('click', ({ target }) => {
+  target.classList.toggle('open');
+});
