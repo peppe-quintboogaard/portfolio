4a18a0191ba758c0608052d9ed0306f2fb500d0e
Peppe-Quint <peppequintb@gmail.com>
2020-10-15 13:13:24 +0200

    smooth scrolling

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,20 @@ const hamburger = document.querySelector('.header-button');
 hamburger.addEventListener('click', ({ target }) => {
   target.classList.toggle('open');
 });
+
+const scroll = document.querySelector('.intro-trigger--link');
+
+scroll.addEventListener('click', smoothScroll);
+
+function smoothScroll(e) {
+  e.preventDefault();
+
+  const href = this.getAttribute('href');
+  const heading = document.querySelector('.work-heading').offsetHeight;
+  const offset = document.querySelector(href).offsetTop - heading;
+
+  window.scroll({
+    top: offset,
+    behavior: 'smooth',
+  });
+}
