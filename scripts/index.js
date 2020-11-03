c1ba4e111968fdc719a148964e0714c313d55c17
Peppe-Quint <peppequintb@gmail.com>
2020-11-03 12:13:42 +0100

    navigation

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,26 @@
-console.log('index.js');
+console.log("index.js");
 
-const hamburger = document.querySelector('.header-button');
+const hamburger = document.querySelector(".header-button");
+const navigation = document.querySelector(".navigation");
 
-hamburger.addEventListener('click', ({ target }) => {
-  target.classList.toggle('open');
+hamburger.addEventListener("click", ({ target }) => {
+  target.classList.toggle("open");
+  navigation.classList.toggle("navigation--open");
 });
 
-const scroll = document.querySelector('.intro-trigger--link');
+const scroll = document.querySelector(".intro-trigger--link");
 
-scroll.addEventListener('click', smoothScroll);
+scroll.addEventListener("click", smoothScroll);
 
 function smoothScroll(e) {
   e.preventDefault();
 
-  const href = this.getAttribute('href');
-  const heading = document.querySelector('.work-heading').offsetHeight;
+  const href = this.getAttribute("href");
+  const heading = document.querySelector(".work-heading").offsetHeight;
   const offset = document.querySelector(href).offsetTop - heading;
 
   window.scroll({
     top: offset,
-    behavior: 'smooth',
+    behavior: "smooth",
   });
 }
